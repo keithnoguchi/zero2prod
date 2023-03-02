5e2f77b36292cb9e5789d072c36a589cbecba09a
Keith Noguchi <keith@noguchi.us>
2023-03-02 07:32:34 -0800

    routes: Hosts the endpoint handler

--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,21 @@
 //!
 //! [zero2prod]: https://github.com/LukeMathWalker/zero-to-production/
 
+mod routes;
+
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
 
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/subscriptions", web::post().to(subscribe))
-            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(routes::subscribe))
+            .route("/health_check", web::get().to(routes::health_check))
     })
     .listen(listener)?
     .run();
 
     Ok(server)
-}
-
-// We'll make those data private once we start to use it inside
-// subscribe function.
-#[derive(serde::Deserialize)]
-pub struct FormData {
-    pub email: String,
-    pub name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
 }
