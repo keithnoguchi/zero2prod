20d14578971505c3ae9bdc14b9f638cbc1b6974e
Keith Noguchi <keith@noguchi.us>
2023-03-01 21:18:01 -0800

    tests: Spawn server under tokio runtime

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -17,5 +17,13 @@ async fn health_check() {
 fn spawn_app() {
     let server = zero2prod::run().expect("failed to bind address");
 
-    let _ = tokio::spawn(server);
+    // Spawn thread to avoid the clippy error.
+    //
+    // https://rust-lang.github.io/rust-clippy/master/index.html#let_underscore_future
+    std::thread::spawn(|| {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(async {
+            let _ = tokio::spawn(server).await;
+        });
+    });
 }
