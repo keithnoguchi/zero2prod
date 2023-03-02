b1c9bc9c0fa6c55fbb4d8a39cbfcb8fddf66065d
Keith Noguchi <keith@noguchi.us>
2023-03-02 07:52:05 -0800

    starutp: Hosts the startup code

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,6 @@
 //! [zero2prod]: https://github.com/LukeMathWalker/zero-to-production/
 
 mod routes;
+mod startup;
 
-use std::net::TcpListener;
-
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
-
-pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/subscriptions", web::post().to(routes::subscribe))
-            .route("/health_check", web::get().to(routes::health_check))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
+pub use startup::run;
