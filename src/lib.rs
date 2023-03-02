361deeb3a91d65e99a751ca3f991bad366b69d7e
Keith Noguchi <keith@noguchi.us>
2023-03-02 07:59:52 -0800

    3.8.5.2 Configuration Management (#1)
    Restructuring before adding the configuration management

--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,7 @@
 //!
 //! [zero2prod]: https://github.com/LukeMathWalker/zero-to-production/
 
-use std::net::TcpListener;
+mod routes;
+mod startup;
 
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
-
-pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/subscriptions", web::post().to(subscribe))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
-
-// We'll make those data private once we start to use it inside
-// subscribe function.
-#[derive(serde::Deserialize)]
-pub struct FormData {
-    pub email: String,
-    pub name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+pub use startup::run;
